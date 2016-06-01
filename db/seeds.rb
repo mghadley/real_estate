e8f38e941834e1b5e59e90c0adad2317c835d5c3
mghadley <matthew.g.hadley@gmail.com>
2016-06-01 09:38:56 -0600

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-obama = User.create(name: 'Obama', phone_number: '111-111-1111', email: 'potus@obamaserver.net', 
+obama = User.find_or_create_by(name: 'Obama', phone_number: '111-111-1111', email: 'potus@obamaserver.net', 
 						password: 'password', password_confirmation: 'password')
-martha = User.create(name: 'Martha Stewart', phone_number: '123-234-3456', email: 'martha@email.com',
+martha = User.find_or_create_by(name: 'Martha Stewart', phone_number: '123-234-3456', email: 'martha@email.com',
 						password: 'password', password_confirmation: 'password')
-jake = User.create(name: 'Jake Sorce', phone_number: '345-456-5678', email: 'jake@email.com',
+jake = User.find_or_create_by(name: 'Jake Sorce', phone_number: '345-456-5678', email: 'jake@email.com',
 						password: 'password', password_confirmation: 'password')
 
-obama.homes.create(title: 'D.C. Mansion', description: 'Large white mansion, good security', sq_ft: 10000,
+obama.homes.find_or_create_by(title: 'D.C. Mansion', description: 'Large white mansion, good security', sq_ft: 10000,
 									 floors: 5, price: 500000)
-martha.homes.create(title: 'Cottage in the Hamptons', description: 'Gorgeous summer getaway',
+martha.homes.find_or_create_by(title: 'Cottage in the Hamptons', description: 'Gorgeous summer getaway',
 										sq_ft: 2500, floors: 1, price: 1000000)
-jake.homes.create(title: 'Sweet Bachelor Pad', description: 'Moving up and out', sq_ft: 800, floors: 1,
+martha.homes.find_or_create_by(title: 'Bungalow', description: 'Well decorated', sq_ft: 1000, floors: 1, price: 200000)
+martha.homes.find_or_create_by(title: '')
+jake.homes.find_or_create_by(title: 'Sweet Bachelor Pad', description: 'Moving up and out', sq_ft: 800, floors: 1,
 									price: 150000)
+
+Address.find_or_create_by(street: 'Some Lane', city: 'Salt Lake City', state: 'utah', zip: '84111', home_id:(Home.find_by(title: 'Sweet Bachelor Pad').id))
